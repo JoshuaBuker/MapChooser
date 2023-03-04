86a7b9d7020fe73bc579d83d687e19ae26e5b00b
JBuke <joshbuker04@gmail.com>
2023-03-04 03:50:19 -0800

    It's 4am. I did stuff

--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,22 +1,37 @@
 import java.awt.*;
-import java.awt.Graphics;
 import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.event.MouseInputListener;
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
 
 
 public class Window  {
 
     private JFrame frame;
     private ImageIcon map;
+    private BufferedImage robot;
+    private BufferedImage cone;
+    private BufferedImage cube;
+    private BufferedImage balance;
+
+    Popup popup;
+    JFrame popupFrame;
+    JLabel popupLabel;
+    JSlider popupSlider;
+
     private JLabel myLabel;
     private JButton buttonCancel;
     private JButton buttonStart;
+    private JButton buttonAngle;
+    private JComboBox<String> eventMenu;
 
     private final double fieldWidth = 16.48969;
     private final double fieldHeight = 8.13816;
@@ -24,6 +39,8 @@ public class Window  {
 
     private final int labelWidth = 1047;
     private final int labelHeight = 516;
+
+    private int currentAngle;
 
     private final double xScalingFactor = fieldWidth / labelWidth;
     private final double yScalingFactor = fieldHeight / labelHeight;
@@ -35,11 +52,56 @@ public class Window  {
     public Window(){
         
         try {
-            map = new ImageIcon(ImageIO.read(new File("img.png")));
+            map = new ImageIcon(ImageIO.read(new File("Resources\\MAP_IMAGE.png")));
+            robot = ImageIO.read(new File("Resources\\ROBOT.png"));
+            cone = ImageIO.read(new File("Resources\\CONE.png"));
+            cube = ImageIO.read(new File("Resources\\CUBE.png"));
+            balance = ImageIO.read(new File("Resources\\BALANCE.png"));
         } catch (IOException e) {
             // TODO Auto-generated catch block
-            System.out.println("Could not find file");
+           new Exception(e);
         }
+
+        // ========== Create Popup Window =============
+        popupFrame = new JFrame();
+        popupLabel = new JLabel(new ImageIcon(robot));
+        popupSlider = new JSlider(0, 360);
+
+        popupLabel.setBounds(100, 50, 200, 200);
+        popupSlider.setBounds(25, 300, 350, 50);
+
+        popupFrame.add(popupLabel);
+        popupFrame.add(popupSlider);
+
+       
+
+        popupFrame.setSize(400, 400);
+        popupFrame.setResizable(false);
+        popupFrame.setLayout(null);
+        popupFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+        popupFrame.setLocationRelativeTo(null);
+        popupFrame.setVisible(false);
+
+
+        popupSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                currentAngle = popupSlider.getValue();
+            }
+            
+        });
+
+
+
+
+        // ========== Populate Event Menu Array ==============
+        String[] eventMenuOptions = {
+            "No Event",
+            "Pickup Cone",
+            "Pickup Cube",
+            "Balance"
+        };
+
         myLabel = new JLabel(map);
         myLabel.setSize(labelWidth, labelHeight);
 
@@ -49,16 +111,28 @@ public class Window  {
         buttonStart.setForeground(Color.BLACK);
         buttonStart.setFocusable(false);
 
+        buttonAngle = new JButton("Set Angle");
+        buttonAngle.setBounds(800, 520, 200, 50);
+        buttonAngle.setBackground(Color.ORANGE);
+        buttonAngle.setForeground(Color.BLACK);
+        buttonAngle.setFocusable(false);
+
         buttonCancel = new JButton("Cancel Current Path");
         buttonCancel.setBounds(550, 520, 200, 50);
         buttonCancel.setBackground(Color.BLACK);
         buttonCancel.setForeground(Color.WHITE);
         buttonCancel.setFocusable(false);
 
+        eventMenu = new JComboBox<String>(eventMenuOptions);
+        eventMenu.setBounds(50, 520, 200, 50);
+        buttonCancel.setFocusable(false);
+
         frame = new JFrame("FRC Map");
         frame.add(myLabel);
         frame.add(buttonStart);
         frame.add(buttonCancel);
+        frame.add(buttonAngle);
+        frame.add(eventMenu);
 
         // ================= BUTTON CONTROLS ===========================
 
@@ -88,6 +162,10 @@ public class Window  {
             arr.clear();
         });
 
+        buttonAngle.addActionListener(e -> {
+            popupFrame.setVisible(true);
+        });
+
 
 
         // ================== MOUSE CONTROLS ==========================
@@ -97,15 +175,38 @@ public class Window  {
            @Override
            public void mouseClicked(java.awt.event.MouseEvent e) {
                Graphics g = myLabel.getGraphics();
-               g.setColor(Color.YELLOW);
                if (pathMode) {
+                    if (eventMenu.getSelectedItem().equals("No Event")) {
+                        g.setColor(Color.YELLOW);
 
-                   arr.add(String.format("X: %f  Y: %f", scaleX(e.getX()), scaleY(e.getY())));
-                   g.fillOval(e.getX() - 10, e.getY() - 10, 20,20);
+                        arr.add(String.format("X: %f  Y: %f  Event: %s  Angle: %d", scaleX(e.getX()), scaleY(e.getY()), eventMenu.getSelectedItem(), currentAngle));
+                        g.fillOval(e.getX() - 10, e.getY() - 10, 20,20);
 
+                    } else if (eventMenu.getSelectedItem().equals("Pickup Cone")) {
 
+                        arr.add(String.format("X: %f  Y: %f  Event: %s  Angle: %d", scaleX(e.getX()), scaleY(e.getY()), eventMenu.getSelectedItem(), currentAngle));
+                        g.setColor(Color.WHITE);
+                        g.fillOval(e.getX() - 17, e.getY() - 17, 34,34);
+                        g.drawImage(cone, e.getX() - 15, e.getY() - 15, null);
+                        eventMenu.setSelectedIndex(0);
+
+                    } else if (eventMenu.getSelectedItem().equals("Pickup Cube")) {
+
+                        arr.add(String.format("X: %f  Y: %f  Event: %s  Angle: %d", scaleX(e.getX()), scaleY(e.getY()), eventMenu.getSelectedItem(), currentAngle));
+                        g.setColor(Color.WHITE);
+                        g.fillOval(e.getX() - 17, e.getY() - 17, 34,34);
+                        g.drawImage(cube, e.getX() - 15, e.getY() - 15, null);
+                        eventMenu.setSelectedIndex(0);
+
+                    } else if (eventMenu.getSelectedItem().equals("Balance")) {
+                        arr.add(String.format("X: %f  Y: %f  Event: %s  Angle: %d", scaleX(e.getX()), scaleY(e.getY()), eventMenu.getSelectedItem(), currentAngle));
+                        g.setColor(Color.WHITE);
+                        g.fillOval(e.getX() - 17, e.getY() - 17, 34,34);
+                        g.drawImage(balance, e.getX() - 15, e.getY() - 15, null);
+                        eventMenu.setSelectedIndex(0);
+                    }
                } else {
-                   System.out.printf("X:%f  Y:%f \n", scaleX(e.getX()), scaleY(e.getY()));
+                   System.out.printf("X: %f  Y: %f  Event: %s  Angle: %d \n", scaleX(e.getX()), scaleY(e.getY()), eventMenu.getSelectedItem(), currentAngle);
                }
            }
 
